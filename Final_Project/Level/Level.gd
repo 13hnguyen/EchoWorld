da55d3b0b81f16aca8ac7f8ee6dbeae2a67167ce
bkrone2021 <bridget.krone@mavs.uta.edu>
2021-10-17 20:56:01 -0500

    skeleton file structure

---
+++ Final_Project/Level/Level.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func _ready() -> void :
+  # We don't want the same random numbers each time.
+  randomize()
+
+# Runs whenever there is input.  This allows us to check for
+#   input when it happens, rather than checking on every frame
+#   in the _physics_process().
+func _input( event : InputEvent ) -> void :
+  # If the user wants out, just end the game.
+  if event.is_action_pressed( "ui_cancel" ) :
+    get_tree().quit( 0 )
