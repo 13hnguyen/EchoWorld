84d4c3bdc6e78961001fe4a30e27f9ac45dbdd36
bkrone2021 <bridget.krone@mavs.uta.edu>
2021-10-29 23:39:22 -0500

    removed esc input event from level. pause scene handles this now

--- Final_Project/Level/Level.gd
+++ Final_Project/Level/Level.gd
@@ -29,11 +29,3 @@ func _ready() -> void :
   
   # Start the background music playing.
   $BackgndMusic.play()
-  
-# Runs whenever there is input.  This allows us to check for
-#   input when it happens, rather than checking on every frame
-#   in the _physics_process().
-#func _input( event : InputEvent ) -> void :
-  # If the user wants out, just end the game.
-#  if event.is_action_pressed( "ui_pause" ) :
-#    get_tree().paused = true
