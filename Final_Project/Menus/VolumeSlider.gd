26468cb40b115c4ef8aea2d39398f45e9a2b9a25
jps5418 <54409845+jps5418@users.noreply.github.com>
2021-11-11 21:03:40 -0600

    Minor volume slider error fixed

--- Final_Project/Menus/VolumeSlider.gd
+++ Final_Project/Menus/VolumeSlider.gd
@@ -12,6 +12,7 @@ onready var _bus := AudioServer.get_bus_index(audio_bus_name)
 func _ready() -> void:
     value = db2linear(AudioServer.get_bus_volume_db(_bus))
     # Replace with function body.
+    
 func _on_value_changed(value: float) -> void:
     AudioServer.set_bus_volume_db(_bus, linear2db(value))
 
