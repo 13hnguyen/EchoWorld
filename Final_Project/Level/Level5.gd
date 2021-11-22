f9d36139118f66ac4ad4780f1b59e623a00648ed
13hnguyen <55261666+13hnguyen@users.noreply.github.com>
2021-11-22 00:36:16 -0600

    tenteye update

---
+++ Final_Project/Level/Level5.gd
@@ -0,0 +1,54 @@
+extends Node
+
+var AsteroidGenInstance = preload("res://FlyingObstacle/L0_asteroid/AsteroidGenerator.tscn")
+var AsteroidGen
+var EnemyGen
+
+var AsteroidGenPlayerDistance = 0.0
+var EnemyGenPlayerDistance = 0.0
+
+func _ready() -> void :
+  # We don't want the same random numbers each time.
+  randomize()
+  var score = get_node("On Screen Labels/PlayerScoreNum")
+  var hp = get_node("On Screen Labels/PlayerHP")
+  score.set_text(str(0))
+  hp.set_text(str(3))
+    
+  if GameData.tryAgain == true:
+    hp.set_text(str(3))
+    GameData.tryAgain = false
+
+  # Start the asteroid generation
+  AsteroidGen = AsteroidGenInstance.instance()
+  add_child(AsteroidGen)
+  AsteroidGen.position = Vector2(1300, -100)
+  
+  AsteroidGenPlayerDistance = AsteroidGen.position.x - $Player.position.x
+  
+  #Start the enemy generation
+  EnemyGen = preload("res://Enemy/BeeEnemy/BeeEnemyGenerator.tscn").instance()
+  EnemyGen.position = Vector2(1800, 700)
+  
+  # Add enemybgenerator to canvas layer since it follows the player  
+  add_child(EnemyGen)
+  
+  EnemyGenPlayerDistance = EnemyGen.position.x - $Player.position.x
+  
+  #Start the rock generation
+  #var RockGen = preload("res://Rock/RockGenerator.tscn")
+  #var rockGen = RockGen.instance()
+  #rockGen.position = Vector2(2000, 870)
+  
+  # Add rock generator to canvas layer since it follows the player
+    
+  #get_child(canvasIndex).add_child(rockGen)
+  
+  
+  # Start the background music playing.
+  $BackgndMusic.play()
+  
+  
+func _process(_delta):
+  AsteroidGen.position.x = $Player.position.x + AsteroidGenPlayerDistance
+  EnemyGen.position.x =    $Player.position.x + EnemyGenPlayerDistance
